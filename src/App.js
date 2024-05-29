b5a9e30d33ff1c00c1f343e737f17f6d5461dc95
meggmumbi <margaretmumbi16@gmail.com>
2024-05-29 13:51:32 +0300

    Landing Page Build Up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import {BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+
 import './App.css';
+import SignUp from './modules/sign-up/SignUp'
+import SignIn from './modules/sign-in/SignIn';
+import LandingPage from './modules/landingPage/LandingPage';
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+       
+        <Route path="/" element={<LandingPage />} />
+        <Route path="sign-in" element={<SignIn />} />
+        <Route path="sign-up" element={<SignUp />} />
+        
+      </Routes>
+    </Router>
   );
 }
+
 
 export default App;
